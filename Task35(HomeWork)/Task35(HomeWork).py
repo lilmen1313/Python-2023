c38e8f1ea1bc0f35321cbe30c2c680f315183a52
lilmen <lilmen1313@gmail.com>
2023-06-15 20:46:47 +0300

    Добавил решение задачи 35 с помощью рекурсии

--- Task35(HomeWork)/Task35(HomeWork).py
+++ Task35(HomeWork)/Task35(HomeWork).py
@@ -6,14 +6,24 @@
 # Input: 5
 # Output: yes
 
-def primeNumber(n):
-    if n > 1:
-        for i in range(2, n):
-            if n % i == 0:
-                print("No")
-                break
-        else:
-            print("Yes")
+# def primeNumber(n):
+#     if n > 1:
+#         for i in range(2, n):
+#             if n % i == 0:
+#                 print("No")
+#                 break
+#         else:
+#             print("Yes")
+
+# n = int(input("Введите число: "))
+# primeNumber(n)
+
+def primeNumbers(n, i = 2):
+    if i != n:
+        if n % i == 0:
+            return "NO"
+        return primeNumbers(n, i + 1)
+    return "YES"
 
 n = int(input("Введите число: "))
-primeNumber(n)
+print(primeNumbers(n))
